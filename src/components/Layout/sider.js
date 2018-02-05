7bf1707236373a68c23978d71599b21dea01924f
Lee <653498671@qq.com>
2018-02-05 17:47:43 +0800

    左边菜单栏

--- src/components/Layout/sider.js
+++ src/components/Layout/sider.js
@@ -1,21 +1,73 @@
 import { Menu, Icon } from 'antd';
+import Menus from "../../menu"
 
 const Siders = () => {
+
+    // const menu = [
+    //     {
+    //         id: "A",
+    //         name: "A",
+    //         children: [
+    //             { id: "A_01", name: "A_01", children: [] },
+    //             { id: "A_02", name: "A_02", children: [] },
+    //         ]
+    //     }, {
+    //         id: "B",
+    //         name: "B",
+    //         children: [
+    //             { id: "B_01", name: "B_01", children: [] },
+    //             { id: "B_02", name: "B_02", children: [] },
+    //         ]
+    //     },
+    // ]
+
+    const menu = listToTree(Menus)
+
+    function listToTree(data) {
+        var tree = [];
+        for (let i = 0, length = data.length; i < length; i++) {
+            let item = data[i];
+            let menuItem = {};
+            menuItem.id = item.id;
+            menuItem.name = item.name;
+            menuItem.icon = item.icon;
+            if (item.children && item.children.length>0) {
+                menuItem.children = [];
+                item.children.map((item,key)=>{
+                    menuItem.children.push({id:item.id,name:item.name,route:item.route})
+                })
+            }
+            tree.push(menuItem);
+        };
+        return tree;
+    }
+
     return (
         <div>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
-                    <Icon type="user" />
-                    <span>nav 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                    <Icon type="video-camera" />
-                    <span>nav 2</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                    <Icon type="upload" />
-                    <span>nav 3</span>
-                </Menu.Item>
+                {
+                    menu.map((item, key) => {
+                        if (item.children ) {
+                            return (
+                                <Menu.SubMenu key={item.id} title={<span><Icon type={item.icon} /><span>{item.name}</span></span>}>
+                                    {item.children.map(itemB => {
+                                        return (
+                                            <Menu.Item key={itemB.id}>
+                                                <span>{itemB.name}</span>
+                                            </Menu.Item>
+                                        )
+                                    })}
+                                </Menu.SubMenu>
+                            )
+                        } else {
+                            return (
+                                <Menu.Item key={item.id}>
+                                    {<span><Icon type={item.icon}/><span>{item.name}</span></span>}
+                                </Menu.Item>
+                            )
+                        }
+                    })
+                }
             </Menu>
         </div>
     )
---
+++ src/menu.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        id: "sysInfo", icon: "user", name: "系统信息", route: "/"
+    },
+    //文章管理
+    {
+        id: "manage", icon: "user", name: "文章管理", children:[
+            {
+                id: "manage_01", pid:"manage", name: "文章列表", route: "articleList"
+            },
+        ]
+    }, 
+    //主页管理
+    {
+        id: "dashboard", icon: "user", name: "主页管理", children:[
+            {
+                id: "dashboard_01", pid:"dashboard",name: "轮播图管理", route: "bannerManage"
+            },
+        ]
+    }, 
+    //用户管理
+    {
+        id: "userManage", icon: "user", name: "用户管理", children:[
+            {
+                id: "userManage_01",pid:"userManage", name: "站内成员", route: "member"
+            },
+        ]
+    },
+]
